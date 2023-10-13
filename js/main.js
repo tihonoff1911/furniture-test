3c5a3ea1721ecacdf6188292c2848944a220bf1e
tihon <tihonoff1911@yandex.ru>
2023-10-13 18:40:25 +0700

    test

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('.video__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+});
+
+
